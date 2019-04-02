a0efb50abb5cc4fdf1aba0d21614cae062dac455
Nikitin Makar <mnayavin@gmail.com>
2019-04-03 01:29:50 +0300

    Небольшое исправление

--- app/src/main/java/com/example/cryptomonitor/database/AppDatabase.java
+++ app/src/main/java/com/example/cryptomonitor/database/AppDatabase.java
@@ -3,7 +3,7 @@ package com.example.cryptomonitor.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {CoinInfo.class}, version = 1)
+@Database(entities = {CoinInfo.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CoinInfoDao coinInfoDao();
 }
