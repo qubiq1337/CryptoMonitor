43d53bb4fa1f851e36c2df1a901806ca493a2efa
Pavel <rjapavel@gmail.com>
2019-04-09 23:53:22 +0300

    Новый model

---
+++ app/src/main/java/com/example/cryptomonitor/model_coinmarket_cup/Quote.java
@@ -0,0 +1,21 @@
+
+package com.example.cryptomonitor.model_coinmarket_cup;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Quote {
+
+    @SerializedName("USD")
+    @Expose
+    private USD uSD;
+
+    public USD getUSD() {
+        return uSD;
+    }
+
+    public void setUSD(USD uSD) {
+        this.uSD = uSD;
+    }
+
+}
