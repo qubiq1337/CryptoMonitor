aa888e074fdd5520e75f8dd06d5a489a8e398d79
Ryzhkov Nikita <nepok1998@mail.ru>
2019-05-14 18:21:56 +0300

    widget's start

---
+++ app/src/main/java/com/example/cryptomonitor/Widget.java
@@ -0,0 +1,27 @@
+package com.example.cryptomonitor;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class Widget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+}
